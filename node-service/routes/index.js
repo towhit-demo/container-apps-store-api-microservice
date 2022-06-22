9f754852587b7370d9fc4401c12ad40dfb907dfb
Todd Whitehead <towhit@microsoft.com>
2022-06-22 13:11:07 +1000

    changed page title

--- node-service/routes/index.js
+++ node-service/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Container App Demo' });
+  res.render('index', { title: 'Container Apps @ NDC Melbourne' });
 });
 
 module.exports = router;
